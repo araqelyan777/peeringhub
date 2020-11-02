c9abc30164d45514c586ed7f216129319be7678d
araqelyan777 <goraraqelyann111@gmail.com>
2020-11-02 17:55:01 +0400

    prod setting

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -13,10 +13,10 @@ module.exports = {
         }
     },
     production:{
-        HOST: "localhost",
-        USER: "postgres",
-        PASSWORD: "admin123",
-        DB: "postgres",
+        HOST: "ec2-34-200-106-49.compute-1.amazonaws.com",
+        USER: "lgjntqjyhtrzwa",
+        PASSWORD: "a389f94455726ee56fee1421ca7d2ceb281539bec71e0d1af59b7277e3691b6c",
+        DB: "d6bp3pq0ephpuo",
         dialect: "postgres",
         pool: {
             max: 5,
